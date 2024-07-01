1438a3d0c51bf7852dd77af5a3fb2e3422d42399
DaChuan <524874035@qq.com>
2024-07-02 03:54:19 +0800

    删除

--- Assets/PixelWeatherFX/Scripts/RotateTornado.js
+++
@@ -1,6 +0,0 @@
-﻿var speed : float = 30.0;
- private var pivot : Vector3 = new Vector3(0,50,0);
- 
- function Update () {
-     transform.RotateAround(pivot, Vector3.up, speed * Time.deltaTime);
- }
